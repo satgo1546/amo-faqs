b4d69c3248b2d3e738abbdbd265385ba84ed720a
Satgo1546 <satgo1546@yeah.net>
2014-02-16 17:28:44 +0800

    The first commit

---
+++ additem.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<?php
+require_once("src/loaded_list.php");
+?>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>AMO FAQs</title>
+        <!-- AMO Custom CSS -->
+        <link href="static/bootstrap.min.css" rel="stylesheet" type="text/css" />
+        <link rel="stylesheet" href="http://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css" />
+        <link rel="stylesheet" href="static/page.css" />
+    </head>
+    <body>
+        <nav class="navbar navbar-default navbar-fixed-top navbar-inverse" role="navigation">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">显示导航栏</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="./">
+                    <img src="static/icon.png" alt="橙光FAQ" width="16" height="16" /> 橙光FAQ
+                </a>
+            </div>
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav">
+                    <li><a href="http://bbs.66rpg.com/thread-324640-1-1.html"><i class="fa fa-arrow-circle-o-left"></i> 返回帖子</a></li>
+                    <li><a href="./"><i class="fa fa-home"></i> 首页</a></li>
+                </ul>
+            </div>
+        </nav>
+        <div class="container">
+            <h1>添加新项目</h1>
+            <p>填写下面的表单后点击“添加”按钮。</p>
+            <form id="newitem" name="newitem" method="post" action="additem.php" role="form">
+                <div class="row">
+                    <div class="col-md-6">
+                        <div class="form-group">
+                            <label for="id">ID</label>
+                            <input type="text" name="id" id="id" class="form-control" />
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="form-group">
+                            <label for="category">分类</label>
+                            <select name="category" id="category" class="form-control">
+                            </select>
+                        </div>
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label for="question">问题</label>
+                    <input type="text" name="question" id="question" class="form-control" />
+                </div>
+                <label>解答</label>
+                <input type="hidden" name="answer" id="answer" />
+                <div id="answer_editor"></div>
+                <button type="submit" class="btn btn-primary" style="margin-top: 8px;">添加</button>
+            </form>
+        </div>
+        <script type="text/javascript" src="http://ajax.microsoft.com/ajax/jquery/jquery-1.10.2.min.js"></script>
+        <script type="text/javascript" src="http://netdna.bootstrapcdn.com/bootstrap/3.0.3/js/bootstrap.min.js"></script>
+        <script type="text/javascript" src="lib/ace-min/ace.js"></script>
+        <script type="text/javascript">
+            var anseditor = ace.edit("answer_editor");
+            anseditor.setTheme("ace/theme/tomorrow");
+            anseditor.getSession().setMode("ace/mode/markdown");
+        </script>
+    </body>
+</html>
---
+++ index.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<?php
+require_once("src/loaded_list.php");
+require_once("lib/markdown/Markdown.inc.php");
+?>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>AMO FAQs</title>
+        <!-- AMO Custom CSS -->
+        <link href="static/bootstrap.min.css" rel="stylesheet" type="text/css" />
+        <link rel="stylesheet" href="http://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css" />
+        <link rel="stylesheet" href="static/page.css" />
+    </head>
+    <body>
+        <nav class="navbar navbar-default navbar-fixed-top navbar-inverse" role="navigation">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">显示导航栏</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="./">
+                    <img src="static/icon.png" alt="橙光FAQ" width="16" height="16" /> 橙光FAQ
+                </a>
+            </div>
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav">
+                    <li><a href="http://bbs.66rpg.com/thread-324640-1-1.html"><i class="fa fa-arrow-circle-o-left"></i> 返回帖子</a></li>
+                    <li class="active"><a href="./"><i class="fa fa-home"></i> 首页</a></li>
+                </ul>
+            </div>
+        </nav>
+        <div class="jumbotron">
+            <div class="container">
+                <h1></h1>
+                <p></p>
+            </div>
+        </div>
+        <div class="container">
+            <div class="faqlist">
+                <?php
+                foreach ($faqxml -> category as $c) {
+                    echo "<h2>$c->name</h2>";
+                    foreach ($c -> faq as $f) {
+                ?>
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        <span class="label label-default"><?php echo $f->id; ?></span>
+                        Q：<?php echo "$f->q"; ?>
+                    </div>
+                    <div class="panel-body">
+                        <?php echo Michelf\Markdown::defaultTransform("A：" . $f->a); ?>
+                    </div>
+                </div>
+                <?php
+                    }
+                }
+                ?>
+            </div>
+        </div>
+        <script type="text/javascript" src="http://ajax.microsoft.com/ajax/jquery/jquery-1.10.2.min.js"></script>
+        <script type="text/javascript" src="http://netdna.bootstrapcdn.com/bootstrap/3.0.3/js/bootstrap.min.js"></script>
+    </body>
+</html>
