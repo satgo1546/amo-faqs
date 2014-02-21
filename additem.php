341eb39b2a38d2c519856d9f4d7405f9f0ee839b
Satgo1546 <satgo1546@yeah.net>
2014-02-21 19:32:09 +0800

    Enabled search
    Now you can search anything in the FAQ list! Yaaaaaaaaaay!

--- additem.php
+++ additem.php
@@ -3,97 +3,98 @@
 require_once("src/loaded_list.php");
 ?>
 <html>
-    <head>
-        <meta charset="UTF-8">
-        <title>AMO FAQs</title>
-        <!-- AMO Custom CSS -->
-        <link href="static/bootstrap.min.css" rel="stylesheet" type="text/css" />
-        <link rel="stylesheet" href="http://cdn.bootcss.com/font-awesome/4.0.3/css/font-awesome.min.css" />
-        <link rel="stylesheet" href="static/page.css" />
-    </head>
-    <body>
-        <nav class="navbar navbar-default navbar-fixed-top navbar-inverse" role="navigation">
-            <div class="navbar-header">
-                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-                    <span class="sr-only">显示导航栏</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-                <a class="navbar-brand" href="./">
-                    <img src="static/icon.png" alt="橙光FAQ" width="16" height="16" /> 橙光FAQ
-                </a>
-            </div>
-            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                <ul class="nav navbar-nav">
-                    <li><a href="http://bbs.66rpg.com/thread-324640-1-1.html"><i class="fa fa-arrow-circle-o-left"></i> 返回帖子</a></li>
-                    <li><a href="./"><i class="fa fa-home"></i> 首页</a></li>
-                </ul>
-            </div>
-        </nav>
-        <div class="container">
-            <h1>添加新项目</h1>
-            <?php
-            if ($_POST["id"]) {
-                foreach ($faqxml->xpath("category[name='" . $_POST["category"] . "']") as $cate) {
-                    $newitem = $cate->addChild("faq");
-                    $newitem->addChild("id", $_POST["id"]);
-                    $newitem->addChild("q", $_POST["question"]);
-                    $newitem->addChild("a", $_POST["answer"]);
-                }
-                $faqxml->saveXML("data/faq_list.xml");
-            ?>
-            <div class="alert alert-success">
-                <p>添加完毕。</p>
-                <p><a href="additem.php" class="alert-link">继续添加</a>或<a href="./" class="alert-link">返回首页</a>。</p>
-            </div>
-            <?php
-            } else {
-            ?>
-            <p>填写下面的表单后点击“添加”按钮。</p>
-            <form id="newitem" name="newitem" method="post" action="additem.php" role="form" autocomplete="off">
-                <div class="row">
-                    <div class="col-md-5">
-                        <div class="form-group">
-                            <label for="id">ID</label>
-                            <input type="text" name="id" id="id" class="form-control" />
-                        </div>
-                    </div>
-                    <div class="col-md-7">
-                        <div class="form-group">
-                            <label for="category">分类</label>
-                            <select name="category" id="category" class="form-control">
-                                <option value="nothing" selected="selected">— 分类 —</option>
-                                <?php
-                                foreach($faqxml->category as $co) {
-                                    echo "<option value=\"$co->name\">$co->name</option>";
-                                }
-                                ?>
-                            </select>
-                        </div>
-                    </div>
-                </div>
-                <div class="form-group">
-                    <label for="question">问题</label>
-                    <input type="text" name="question" id="question" class="form-control" />
-                </div>
-                <label>解答</label>
-                <input type="hidden" name="answer" id="answer" />
-                <div id="answer_editor"></div>
-                <button type="submit" class="btn btn-primary" style="margin-top: 8px;" onclick="additemfunc();">添加</button>
-            </form>
-            <?php
-            }
-            ?>
-        </div>
-        <script type="text/javascript" src="http://cdn.bootcss.com/jquery/1.10.2/jquery.min.js"></script>
-        <script type="text/javascript" src="http://cdn.bootcss.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
-        <script type="text/javascript" src="lib/ace-min/ace.js"></script>
-        <script type="text/javascript">
-            var anseditor = ace.edit("answer_editor");
-            anseditor.setTheme("ace/theme/tomorrow");
-            anseditor.getSession().setMode("ace/mode/markdown");
-        </script>
-        <script type="text/javascript" src="src/functions.js"></script>
-    </body>
+	<head>
+		<meta charset="UTF-8">
+		<title>AMO FAQs</title>
+		<!-- AMO Custom CSS -->
+		<link href="static/bootstrap.min.css" rel="stylesheet" type="text/css" />
+		<link rel="stylesheet" href="http://cdn.bootcss.com/font-awesome/4.0.3/css/font-awesome.min.css" />
+		<link rel="stylesheet" href="static/page.css" />
+	</head>
+	<body>
+		<nav class="navbar navbar-default navbar-fixed-top navbar-inverse" role="navigation">
+			<div class="navbar-header">
+				<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+					<span class="sr-only">显示导航栏</span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</button>
+				<a class="navbar-brand" href="./">
+					<img src="static/icon.png" alt="橙光FAQ" width="16" height="16" /> 橙光FAQ
+				</a>
+			</div>
+			<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+				<ul class="nav navbar-nav">
+					<li><a href="http://bbs.66rpg.com/thread-324640-1-1.html"><i class="fa fa-arrow-circle-o-left"></i> 返回帖子</a></li>
+					<li><a href="./"><i class="fa fa-home"></i> 首页</a></li>
+				</ul>
+			</div>
+		</nav>
+		<div class="container">
+			<h1>添加新项目</h1>
+			<?php
+			if ($_POST["id"]) {
+				foreach ($faqxml->xpath("category[name='{$_POST["category"]}']") as $cate) {
+					$newitem = $cate->addChild("faq");
+					$newitem->addChild("id", $_POST["id"]);
+					$newitem->addChild("q", $_POST["question"]);
+					$newitem->addChild("a", $_POST["answer"]);
+				}
+				$faqxml->saveXML("data/faq_list.xml");
+				?>
+				<div class="alert alert-success">
+					<p>添加完毕。</p>
+					<p><a href="additem.php" class="alert-link">继续添加</a>或<a href="./" class="alert-link">返回首页</a>。</p>
+				</div>
+				<?php
+			} else {
+				?>
+				<p>填写下面的表单后点击“添加”按钮。</p>
+				<form id="newitem" name="newitem" method="post" action="additem.php" role="form" autocomplete="off">
+					<div class="row">
+						<div class="col-md-5">
+							<div class="form-group">
+								<label for="id">ID</label>
+								<input type="text" name="id" id="id" class="form-control" />
+							</div>
+						</div>
+						<div class="col-md-7">
+							<div class="form-group">
+								<label for="category">分类</label>
+								<select name="category" id="category" class="form-control">
+									<option value="nothing" selected="selected">— 分类 —</option>
+									<?php
+									foreach ($faqxml->category as $co) {
+										echo "<option value=\"$co->name\">$co->name</option>";
+									}
+									?>
+								</select>
+							</div>
+						</div>
+					</div>
+					<div class="form-group">
+						<label for="question">问题</label>
+						<input type="text" name="question" id="question" class="form-control" />
+					</div>
+					<label>解答</label>
+					<input type="hidden" name="answer" id="answer" />
+					<div id="answer_editor"></div>
+					<span class="help-block">为了实现 Q&quot;A 的效果，在执行 Markdown 解释前会自动在第一行之前加入“A：”。</span>
+					<button type="submit" class="btn btn-primary" style="margin-top: 8px;" onclick="additemfunc();">添加</button>
+				</form>
+				<?php
+			}
+			?>
+		</div>
+		<script type="text/javascript" src="http://cdn.bootcss.com/jquery/1.10.2/jquery.min.js"></script>
+		<script type="text/javascript" src="http://cdn.bootcss.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
+		<script type="text/javascript" src="lib/ace-min/ace.js"></script>
+		<script type="text/javascript">
+					var anseditor = ace.edit("answer_editor");
+					anseditor.setTheme("ace/theme/tomorrow");
+					anseditor.getSession().setMode("ace/mode/markdown");
+		</script>
+		<script type="text/javascript" src="src/functions.js"></script>
+	</body>
 </html>
--- search.php
+++ search.php
@@ -28,58 +28,56 @@ require_once("lib/markdown/Markdown.inc.php");
 			<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
 				<ul class="nav navbar-nav">
 					<li><a href="http://bbs.66rpg.com/thread-324640-1-1.html"><i class="fa fa-arrow-circle-o-left"></i> 返回帖子</a></li>
-					<li class="active"><a href="./"><i class="fa fa-home"></i> 首页</a></li>
+					<li><a href="./"><i class="fa fa-home"></i> 首页</a></li>
 				</ul>
 			</div>
 		</nav>
-		<div class="jumbotron">
-			<div class="container">
-				<h1></h1>
-				<?php echo $faqxml->searchtip; ?>
-				<div class="row">
-					<form id="searchfaq" name="searchfaq" method="get" action="search.php" role="form" autocomplete="off">
-						<div class="col-xs-12 col-md-9">
-							<label for="q" class="sr-only">输入你要查找的问题的关键字</label>
-							<div class="input-group input-group-lg">
-								<input type="text" name="q" id="q" class="form-control input-lg" placeholder="输入你要查找的问题的关键字" />
-								<span class="input-group-btn">
-									<button type="submit" class="btn btn-primary btn-block btn-lg searchbtn">搜索</button>
-								</span>
-							</div>
+		<div class="container">
+			<h1>搜索结果</h1>
+			<div class="row">
+				<form id="searchfaq" name="searchfaq" method="get" action="search.php" role="form" autocomplete="off">
+					<div class="col-md-10">
+						<label for="q" class="sr-only">输入你要查找的问题的关键字</label>
+						<div class="input-group">
+							<input type="text" name="q" id="q" class="form-control" placeholder="输入你要查找的问题的关键字" value="<?php echo $_GET["q"]; ?>" />
+							<span class="input-group-btn">
+								<button type="submit" class="btn btn-primary btn-block searchbtn">搜索</button>
+							</span>
 						</div>
-					</form>
-					<div class="col-xs-12 col-md-3">
-						<button type="button" class="btn btn-default btn-lg btn-block" onclick="toggleallfaqs();">展开/收起全部问题</button>
 					</div>
+				</form>
+				<div class="col-md-2">
+					<button type="button" class="btn btn-default btn-block" onclick="toggleallfaqs();">展开/收起全部问题</button>
 				</div>
 			</div>
-		</div>
-		<div class="container">
-			<blockquote>
-				<p><?php echo $faqxml->satgo ?></p>
-				<small><cite title="satgo">satgo</cite></small>
-			</blockquote>
 			<div class="faqlist" id="faqlist">
 				<?php
 				foreach ($faqxml->category as $c) {
 					echo "<hr />";
 					echo "<h2><span class=\"label label-primary\">C-$c->id</span> $c->name</h2>";
+					$found = FALSE;
 					foreach ($c->faq as $f) {
-						?>
-						<div class="panel panel-default">
-							<div class="panel-heading">
-								<span class="label label-default"><?php echo $f->id; ?></span>
-								<a data-toggle="collapse" href="#<?php echo strtolower($f->id) ?>">
-									Q：<?php echo "$f->q"; ?>
-								</a>
-							</div>
-							<div class="panel-collapse collapse" id="<?php echo strtolower($f->id) ?>">
-								<div class="panel-body">
-									<?php echo Michelf\Markdown::defaultTransform("A：" . $f->a); ?>
+						if ((strpos((string) $f->q, $_GET["q"]) !== FALSE) || (strpos((string) $f->q, $_GET["q"]) !== FALSE)) {
+							$found = TRUE;
+							?>
+							<div class="panel panel-default">
+								<div class="panel-heading">
+									<span class="label label-default"><?php echo $f->id; ?></span>
+									<a data-toggle="collapse" href="#<?php echo strtolower($f->id) ?>">
+										Q：<?php echo "$f->q"; ?>
+									</a>
+								</div>
+								<div class="panel-collapse collapse in" id="<?php echo strtolower($f->id) ?>">
+									<div class="panel-body">
+										<?php echo Michelf\Markdown::defaultTransform("A：" . $f->a); ?>
+									</div>
 								</div>
 							</div>
-						</div>
-						<?php
+							<?php
+						}
+					}
+					if ($found == FALSE) {
+						echo "在此分类下没有找到匹配项。";
 					}
 				}
 				?>
